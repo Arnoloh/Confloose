01940cb1f6953dc2557d684632172c494bdadf35
Arnoloh's Mac <nono.corcione@gmail.com>
2022-11-02 22:24:03 +0100

    auto submission

---
+++ renameall.py
@@ -0,0 +1,20 @@
+import os
+def rename(way):
+    directory=os.listdir(way)
+    for dir in directory:
+        if dir in exclu:
+            continue
+        if not os.path.isfile(f"{way}/{dir}"):
+            rename(f"{way}{dir}/")
+        os.system(f"mv {way}{dir} {way}.5{dir}")
+    return 1
+
+
+exclu=[".confs",".DS_Store",".","..","renameall.py","inverse.py"]
+path="/Users/arnoloh/Desktop/con/"
+rename(path)
+os.system("tree -a")
+
+
+
+
